4b7bc0e2adbf94e652d8c61979efdd41935b5a99
Izaac Lim <izaaclim@Izaacs-MacBook-Pro.local>
2026-04-16 18:58:06 +0100

    Fix base path to match repo name for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/imc-prosperity-3-visualizer/',
+  base: '/IMC-Prosperity-Visualiser/',
   build: {
     minify: false,
     sourcemap: true,
